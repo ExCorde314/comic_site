a432109ca74f8c36787477a27819652573c30f7f
Jonathan Lowe <jonathanglowe@gmail.com>
2018-01-03 14:05:04 -0500

    Added bootstrap scss to project

--- comic_site/add_ppa.sh
+++
@@ -1,7 +0,0 @@
-if grep -Fxq "deb http://ftp.debian.org/debian jessie-backports main" /etc/apt/sources.list 
-then
-    echo "PPA already exists" 
-else
-    echo "Added PPA"
-    echo 'deb http://ftp.debian.org/debian jessie-backports main' >> /etc/apt/sources.list
-fi
